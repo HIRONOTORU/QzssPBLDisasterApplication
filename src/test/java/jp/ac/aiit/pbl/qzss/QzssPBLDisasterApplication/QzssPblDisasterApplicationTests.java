3894c3514ebb00a221fe0f4e7037918cd88dda40
a18z4th@aiit.ac.jp <a18z4th@icloud.com>
2020-09-04 18:30:49 +0900

    test code delete at maven.yml

--- src/test/java/jp/ac/aiit/pbl/qzss/QzssPBLDisasterApplication/QzssPblDisasterApplicationTests.java
+++ src/test/java/jp/ac/aiit/pbl/qzss/QzssPBLDisasterApplication/QzssPblDisasterApplicationTests.java
@@ -3,11 +3,14 @@ package jp.ac.aiit.pbl.qzss.QzssPBLDisasterApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class QzssPblDisasterApplicationTests {
-/*
+
 	@Test
 	void contextLoads() {
+	      assertEquals( true, true);
+
 	}
-*/
 }
